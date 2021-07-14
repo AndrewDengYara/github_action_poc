7cf50f7c3b955cd9f9ac899fd6c0998cefa4b8ee
Andrew <kobe73er@gmail.com>
2021-07-14 17:13:32 +0800

    init

---
+++ src/main/java/com/ad/githubactionpoc/GithubactionpocApplication.java
@@ -0,0 +1,13 @@
+package com.ad.githubactionpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionpocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GithubactionpocApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ad/githubactionpoc/GithubactionpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.githubactionpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionpocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
